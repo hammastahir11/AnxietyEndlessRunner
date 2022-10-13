889e7379c3c552caee57db03d598e91e1e992aca
kashirao147 <kashidevlab@gmail.com>
2022-10-14 02:11:19 +0500

    NewParallax

--- Assets/Scripts/repeatebackground.cs
+++ Assets/Scripts/repeatebackground.cs
@@ -1,17 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class repeatebackground : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    [SerializeField] GameObject map;
+    //[SerializeField] GameObject mapGenKey;
+    Transform obj;
+    private void OnTriggerExit2D(Collider2D collision)
     {
         //Destroy(collision.gameObject);
         if (collision.gameObject.tag.Equals("Player"))
         {
+            Vector3 newPosition = new Vector3(19.1599998f, 0, 0) + map.transform.position;
+            obj = map.transform;
+            obj.position = newPosition;
+            Debug.Log(newPosition);
+            Instantiate(gameObject,obj);
 
-            //Hammas Kuta zaleel ... zaleela nalu zaleel
-            collision.gameObject.transform.position =  new Vector3(-9.23f, -3.86f, 0);
+            Vector3 TempVec= new Vector3(gameObject.transform.position.x, 0, 0);
+            gameObject.transform.position += TempVec ;
+
+
+
+            //Destroy(collision.gameObject);
+            //collision.gameObject.transform.position =  new Vector3(-9.23f, -3.86f, 0);
 
         }
     }
