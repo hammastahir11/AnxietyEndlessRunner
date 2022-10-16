2bfb5a99fa8a38d8f094a65bf8a2d8c883f66302
Hammas Tahir <hammastahir11@gmail.com>
2022-10-16 14:03:52 +0500

    Menus

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pauseMenuScreen;
+    
+    // public GameObject obj;
+    // public PlayerMovement pm;
+    // [SerializeField] PlayerMovement playerMovement;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // obj=GameObject.FindGameObjectWithTag("Player");   
+        // pm=obj.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void PauseGame(){
+        Time.timeScale=0;
+        pauseMenuScreen.SetActive(true);     
+    }
+
+    public void ResumeGame(){
+        Time.timeScale=1;
+        pauseMenuScreen.SetActive(false);
+
+    }
+
+    public void goToMenu(){
+
+    }
+}
